e28fe722eb363f5b0373992db678845d4b9eeb4a
Thomas Schuering <thomsch98@gmail.com>
2017-04-22 15:19:55 +0200

    Update docker-entrypoint.sh
    Added missing "-" for correct processing of JAVA_OPTS defined by "the environment" (e.g. via docker run -e JAVA_OPTS=-Dapis)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 codegen="/swagger-api/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
 
-JAVA_OPTS=${JAVA_OPTS:"-XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"}
+JAVA_OPTS=${JAVA_OPTS:-"-XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"}
 
 case "$1" in
     generate|help|langs|meta|config-help|version)
